50d457e99b09a0c3a9232be08acca6dc28951dd2
Tony-Elia <antony.safaga@gmail.com>
2024-09-17 15:03:06 -0400

    TODO 3: Array adapter

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -2,8 +2,11 @@ package edu.temple.inclassuiacvitivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
+import java.util.Arrays
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +17,9 @@ class MainActivity : AppCompatActivity() {
 
         /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
         val numberArray = Array<Int>(19) { i -> i * 5 + 5 }
-
+        Log.i("Number Array", Arrays.toString(numberArray))
         /* TODO Step 3: Create adapter to display items from array in Spinner */
-//        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, numberArray)
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, numberArray)
 
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
