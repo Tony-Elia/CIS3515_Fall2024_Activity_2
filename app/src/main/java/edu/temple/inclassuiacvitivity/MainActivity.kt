69c0a9ca68f1da8c20d9f6483085e2a5d1cc99dd
Tony-Elia <antony.safaga@gmail.com>
2024-09-17 15:11:58 -0400

    TODO 1: Views constrained

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -16,14 +16,25 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
-        val numberArray = Array<Int>(19) { i -> i * 5 + 5 }
+        val numberArray = Array<Int>(20) { i -> i * 5 + 5 }
         Log.i("Number Array", Arrays.toString(numberArray))
         /* TODO Step 3: Create adapter to display items from array in Spinner */
         spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, numberArray)
 
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                // Get the selected size from the spinner
+                val selectedSize = numberArray[position]
 
+                // Change the text size of the TextView to the selected size
+                displayTextView.textSize = selectedSize.toFloat()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing if no item is selected
+            }
+        }
     }
 }
